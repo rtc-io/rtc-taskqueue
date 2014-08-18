c9329ce04a85486325ee66ff7208e15e9e114d9e
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-18 14:26:22 +1000

    Finalise tests

--- test/couple.js
+++ test/couple.js
@@ -1,10 +1,13 @@
 var test = require('tape');
 var taskqueue = require('..');
 var RTCPeerConnection = require('rtc-core/detect')('RTCPeerConnection');
+var waitConnected = require('rtc-core/wait-connected');
 var connections = [];
 var queues = [];
+var offerSdp;
+var answerSdp;
 
-require('cog/logger').enable('*');
+// require('cog/logger').enable('*');
 
 test('can create connection:0', function(t) {
   t.plan(1);
@@ -23,7 +26,37 @@ test('can wrap the connections in queues', function(t) {
   t.ok(queues[1]);
 });
 
+test('connect icecandidate event listeners so candidates are exchanged', function(t) {
+  t.plan(1);
+  connections[0].onicecandidate = queues[1].addIceCandidate;
+  connections[1].onicecandidate = queues[0].addIceCandidate;
+  t.pass('applied');
+});
+
 test('can create an offer using queue:0', function(t) {
   t.plan(1);
+  queues[0].once('sdp', function(sdp) {
+    t.ok(sdp, 'got sdp');
+    offerSdp = sdp;
+  });
+
   queues[0].createOffer();
+});
+
+test('can setRemoteDescription on connection:1', function(t) {
+  t.plan(1);
+  queues[1].once('sdp', function(sdp) {
+    answerSdp = sdp;
+    t.ok(sdp, 'got sdp');
+  });
+
+  queues[1].setRemoteDescription(offerSdp);
+});
+
+test('can setRemoteDescription on connection:0', function(t) {
+  t.plan(2);
+  waitConnected(connections[0], t.pass.bind(t, 'connection:0 connected'));
+  waitConnected(connections[1], t.pass.bind(t, 'connection:1 connected'));
+
+  queues[0].setRemoteDescription(answerSdp);
 });
